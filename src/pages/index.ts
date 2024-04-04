11bd44733052bf2368e4919d7fdc1169a5871c76
이운린 <105215297+likegitman@users.noreply.github.com>
2024-04-04 21:13:03 +0900

    Update route

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
 export { default as SignInPage } from "./signin";
+export { default as MainPage } from "./main";
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,13 +1,11 @@
-import { useNavigate } from "react-router-dom";
-import {gauthUri} from "@/libs";
 import { useLogin } from "@/hooks";
+import { gauthUri } from "@/libs";
 
 function SignInPage() {
   useLogin()
-  const navigate = useNavigate();
   return (
     <div>
-      <button onClick={() => navigate(gauthUri)}>GAuth Button</button>
+      <button onClick={() => window.location.href = gauthUri}>GAuth Button</button>
     </div>
   );
 }
