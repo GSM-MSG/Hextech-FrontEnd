efd570a88c2a24a57f2afae3bf2244e67f5acbec
송현우 <shw9052@naver.com>
2024-04-08 20:11:01 +0900

    Add gauth login btn page publish

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,11 +1,23 @@
 import { useLogin } from "@/hooks";
 import { gauthUri } from "@/libs";
+import * as S from "./style.css";
+import { SignInTitle } from "@/assets";
 
 function SignInPage() {
-  useLogin()
+  useLogin();
   return (
-    <div>
-      <button onClick={() => window.location.href = gauthUri}>GAuth Button</button>
+    <div className={S.SignInPageWrapper}>
+      <div className={S.BackgroundCover}>
+        <div className={S.SignInBox}>
+          <SignInTitle />
+          <button
+            className={S.GAuthButton}
+            onClick={() => (window.location.href = gauthUri)}
+          >
+            GAuth로 로그인
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
